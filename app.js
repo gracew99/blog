b7d3a2a951fbcad956e40824e4633e277840491f
Grace W <gracew@GW-MacBook-Pro.local>
2020-05-25 19:55:08 -0400

    comment out sendgrid for aws hosting debugging

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var _ = require('lodash');
 var mongoose = require('mongoose');
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
  
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -119,21 +119,21 @@ app.post("/compose", function(req, res){
 
   });
 
-  const msg = {
-    to: [
-      {email: '7.knicksfan.7@gmail.com'}, 
-      {email: 'gw297@scarletmail.rutgers.edu'}],
-    from: '7.knicksfan.7@gmail.com',
-    subject: req.body.title,
-    text: req.body.post
-  };
-  sgMail.send(msg, function(err){
-    if (err)
-      console.log(err);
-    else
-      console.log("sent");
+  // const msg = {
+  //   to: [
+  //     {email: '7.knicksfan.7@gmail.com'}, 
+  //     {email: 'gw297@scarletmail.rutgers.edu'}],
+  //   from: '7.knicksfan.7@gmail.com',
+  //   subject: req.body.title,
+  //   text: req.body.post
+  // };
+  // sgMail.send(msg, function(err){
+  //   if (err)
+  //     console.log(err);
+  //   else
+  //     console.log("sent");
 
-  });
+  // });
   
 
 });
